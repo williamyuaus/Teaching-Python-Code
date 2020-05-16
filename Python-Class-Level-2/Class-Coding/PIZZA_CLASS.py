cf7c6d68492b9c0140ad7ce43a4f4b9b11c00e54
williamyuaus <williamyuaus@gmail.com>
2020-05-16 18:00:05 +1000

    class code updated

--- Python-Class-Level-2/Class-Coding/PIZZA_CLASS.py
+++ Python-Class-Level-2/Class-Coding/PIZZA_CLASS.py
@@ -34,5 +34,28 @@ def draw_circle(radius, line_color, fill_color):
 	my_turtle.circle(radius)
 	my_turtle.end_fill()
 
+def move_turtle(x, y):
+    my_turtle.up()
+    my_turtle.goto(x, y)
+    my_turtle.down()
+
 draw_circle(150, CRUST_COLOR, CRUST_COLOR)
+move_turtle(0, 25)
 draw_circle(125, SAUCE_COLOR, CHEESE_COLOR)
+
+for location in PEPPERONI_LOCATIONS:
+    move_turtle(location[0], location[1])
+    draw_circle(10, SAUCE_COLOR, SAUCE_COLOR)
+
+move_turtle(0, 150)
+my_turtle.color(BACKGROUND_COLOR)
+
+for x in range(0, 8):
+    my_turtle.pendown()
+    my_turtle.left(45)
+    my_turtle.forward(150)
+    my_turtle.penup()
+    my_turtle.backward(150)
+
+
+
