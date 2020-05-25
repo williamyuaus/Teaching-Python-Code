893c07958d676fa07291dd4442cb15d1df25de76
williamyuaus <williamyuaus@gmail.com>
2020-05-25 17:49:30 +1000

    update class code

--- Python-Class-Level-3/Class-Coding/bounce_class.py
+++ Python-Class-Level-3/Class-Coding/bounce_class.py
@@ -12,18 +12,66 @@ tk.update()
 
 
 class Ball:
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, paddle, color):
         self.canvas = canvas
+        self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
+        starts = [-3, -2, -1, 1, 2, 3]
+        random.shuffle(starts)
+        self.x = starts[0]
+        self.y = -3
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
 
     def draw(self):
-        self.canvas.move(self.id, 0, -1)
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 1
+        if pos[3] >= self.canvas_height:
+            self.y = -1
+        if self.hit_paddle(pos) == True:
+            self.y = -1
+        if pos[0] <= 0:
+            self.x = 1
+        if pos[2] >= self.canvas_width:
+            self.x = -1
 
-ball = Ball(canvas, 'red')
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        
+class Paddle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 200, 300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+
+    def turn_left(self, evt):
+        self.x = -2
+
+    def turn_right(self, evt):
+        self.x = 2
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        if pos[2] >= self.canvas_width:
+            self.x = 0
+            
+paddle = Paddle(canvas, 'blue')
+ball = Ball(canvas, paddle, 'red')
+
 
 while 1:
     ball.draw()
+    paddle.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
