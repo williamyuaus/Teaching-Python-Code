4f4f0e87c505c0e4d0dc4f5b8299873bf639b6ae
williamyuaus <williamyuaus@gmail.com>
2020-06-03 18:28:54 +1000

    update class code

--- Python-Class-Level-2/Class-Coding/PIZZA_CLASS_Wed.py
+++ Python-Class-Level-2/Class-Coding/PIZZA_CLASS_Wed.py
@@ -5,7 +5,18 @@ CRUST_COLOR = "#ECA84F"
 SAUCE_COLOR = "#AD0509"
 CHEESE_COLOR = "#FBC70F"
 PEPPERONI_LOCATIONS = [
-    [-70, 105]
+    [-70, 105],
+    [-85, 175],
+    [-25, 50],
+    [-15, 100],
+    [-25, 150],
+    [-30, 205],
+    [15, 50],
+    [20, 200],
+    [60, 156],
+    [71, 215],
+    [80, 90],
+    [95, 150]
 ]
 
 screen = turtle.Screen()
@@ -23,4 +34,30 @@ def draw_circle(radius, line_color, fill_color):
     my_turtle.circle(radius)
     my_turtle.end_fill()
 
+def move_turtle(x, y):
+    my_turtle.up()
+    my_turtle.goto(x, y)
+    my_turtle.down()
+
 draw_circle(150, CRUST_COLOR, CRUST_COLOR)
+move_turtle(0, 25)
+draw_circle(125, SAUCE_COLOR, CHEESE_COLOR)
+
+for location in PEPPERONI_LOCATIONS:
+    move_turtle(location[0], location[1])
+    draw_circle(10, SAUCE_COLOR, SAUCE_COLOR)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
