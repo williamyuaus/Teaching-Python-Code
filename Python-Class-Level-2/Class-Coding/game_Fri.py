f48264fdfa1c173187a96bdee11de798146c6555
williamyuaus <williamyuaus@gmail.com>
2020-06-05 17:38:41 +1000

    update class code

---
+++ Python-Class-Level-2/Class-Coding/game_Fri.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+tk = Tk()
+tk.title("Game")
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
