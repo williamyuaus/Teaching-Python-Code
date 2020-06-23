2cf59b23445bf7cca80c105fdfb88a66b3b85fae
williamyuaus <williamyuaus@gmail.com>
2020-06-23 15:19:35 +1000

    update class code

--- Python-Class-Level-2/Class-Coding/bounce_Sat.py
+++ Python-Class-Level-2/Class-Coding/bounce_Sat.py
@@ -11,12 +11,15 @@ canvas.pack()
 tk.update()
 
 class Ball:
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, paddle, color):
         self.canvas = canvas
+        self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
-        self.x = 3
-        self.y = -3
+        starts = [-3, -2, -1, 1, 2, 3]
+        random.shuffle(starts)
+        self.x = starts[0]
+        self.y = starts[1]
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
 
@@ -31,13 +34,58 @@ class Ball:
             self.x = 1
         if pos[2] >= self.canvas_width:
             self.x = -1
-        
 
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1]:
+                return True
+        return False
+
+class Paddle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 200, 300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+
+    def turn_left(self, evt):
+        self.x = -2
+        
+    def turn_right(self, evt):
+        self.x = 2
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
+
+        
+paddle = Paddle(canvas, 'blue')
 ball = Ball(canvas, 'red')
 
 
 while 1:
     ball.draw()
+    paddle.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
+
+
+
+
+
+
+
+
+
+
+
+
