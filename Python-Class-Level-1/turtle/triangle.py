cd80107880126a7257c6192824e3c1691767f6d5
williamyuaus <williamyuaus@gmail.com>
2020-07-02 13:29:30 +1000

    update sample code

---
+++ Python-Class-Level-1/turtle/triangle.py
@@ -0,0 +1,9 @@
+import turtle
+
+t = turtle.Pen()
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+
---
+++ Python-Class-Level-4/transipotationCipher.py
@@ -0,0 +1,46 @@
+# Transposition Cipher Encryption
+# https://www.nostarch.com/crackingcodes (BSD Licensed)
+
+import pyperclip
+
+def main():
+    myMessage = 'Weibo is good at programming.'
+    myKey = 8
+
+    ciphertext = encryptMessage(myKey, myMessage)
+
+    # Print the encrypted string in ciphertext to the screen, with
+    # a | ("pipe" character) after it in case there are spaces at
+    # the end of the encrypted message.
+    print(ciphertext + '|')
+    print(len(myMessage))
+
+    # Copy the encrypted string in ciphertext to the clipboard.
+    pyperclip.copy(ciphertext)
+
+
+def encryptMessage(key, message):
+    # Each string in ciphertext represents a column in the grid.
+    ciphertext = [''] * key
+
+    # Loop through each column in ciphertext.
+    for column in range(key):
+        currentIndex = column
+
+        # Keep looping until currentIndex goes past the message length.
+        while currentIndex < len(message):
+            # Place the character at currentIndex in message at the
+            # end of the current column in the ciphertext list.
+            ciphertext[column] += message[currentIndex]
+
+            # move currentIndex over
+            currentIndex += key
+
+    # Convert the ciphertext list into a single string value and return it.
+    return ''.join(ciphertext)
+
+
+# If transpositionEncrypt.py is run (instead of imported as a module) call
+# the main() function.
+if __name__ == '__main__':
+    main()
