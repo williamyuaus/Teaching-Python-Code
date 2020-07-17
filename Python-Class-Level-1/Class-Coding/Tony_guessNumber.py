3c0760e14ac3cfa12876d84085c8cc28f62048ce
williamyuaus <williamyuaus@gmail.com>
2020-07-17 17:33:35 +1000

    update class code

--- Python-Class-Level-1/Class-Coding/Tony_guessNumber.py
+++ Python-Class-Level-1/Class-Coding/Tony_guessNumber.py
@@ -7,4 +7,27 @@ myName = input()
 
 number = random.randint(1, 20)
 print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
-print(number)
+
+for guessesTaken in range(6):
+    print('Take a guess.')
+    guess = input()
+    guess = int(guess)
+
+    if (guess < number):
+        print('lower')
+
+    if (guess > number):
+        print('higher')
+
+    if (guess == number):
+        break
+
+if (guess == number):
+    guessesTaken = str(guessesTaken + 1)
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+
+if (guess != number):
+    number = str(number)
+    print('Nope. The number was ' + number + '.')
+
+    
--- Python-Class-Level-2/Class-Coding/game_Fri.py
+++ Python-Class-Level-2/Class-Coding/game_Fri.py
@@ -11,8 +11,9 @@ canvas.pack()
 tk.update()
 
 class Ball:
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, paddle, color):
         self.canvas = canvas
+        self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
         self.x = 3
@@ -27,15 +28,55 @@ class Ball:
             self.y = 3
         if pos[3] >= self.canvas_height:
             self.y = -3
+        if self.hit_paddle(pos) == True:
+            self.y = -3
         if pos[0] <= 0:
             self.x = 3
         if pos[2] >= self.canvas_width:
             self.x = -3
 
-ball = Ball(canvas, 'red')
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                return True
+        return False
+
+class Paddle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 200, 300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+
+    def turn_left(self, evt):
+        self.x = -2
+
+    def turn_right(self, evt):
+        self.x = 2
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
+
+paddle = Paddle(canvas, 'blue')
+ball = Ball(canvas, paddle, 'red')
 
 while 1:
     ball.draw()
+    paddle.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
+
+
+
+
+
