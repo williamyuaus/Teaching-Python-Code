0db3b38da31225b2902afd8f9303f2ac8fdad030
williamyuaus <williamyuaus@gmail.com>
2020-07-18 18:05:54 +1000

    update class code

--- Python-Class-Level-3/magicians.py
+++ Python-Class-Level-3/magicians.py
@@ -1,7 +1,3 @@
 magicians = ['Alice', 'David', 'Carolina', 'Albert','Weibo']
 for magician in magicians:
-    # print(f"{magician.title()}, that was a great trick!")
-
-for x in Magicians:
-    print ("%s that was a great trick!" % (Magicians [i]))
-    i = i + 1
+    print(f"{magician.title()}, that was a great trick!")
