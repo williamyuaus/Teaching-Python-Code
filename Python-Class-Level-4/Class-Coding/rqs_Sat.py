5b5414355381ac538db24a8186407263a62b6481
williamyuaus <williamyuaus@gmail.com>
2020-07-21 11:49:09 +1000

    update class code

--- Python-Class-Level-4/Class-Coding/rqs_Sat.py
+++ Python-Class-Level-4/Class-Coding/rqs_Sat.py
@@ -13,17 +13,37 @@ while True:
     print('%s Wins, %s Losses, %s Ties' %(wins, losses, ties))
     while True:
         print('Enter your move: (r)ock (p)aper (s)cissors or (q)uit')
-        playerMove = input()
+        playerMove = input().lower()
         if playerMove == 'q':
             sys.exit()
-        
+        if playerMove =='r' or playerMove == 'p' or playerMove =='s':
+            break
+        print("Type one of r, p, s, or q.")
+
+
+    # Display the player move
+    if playerMove == 'r':
+        print('ROCK versus...')
+    elif playerMove == 'p':
+        print('PAPER versus...')
+    elif playerMove == 's':
+        print('SCISSORS versus...')
+
+
+    # Display the computer move
+    randomNumber = random.randint(1, 3)
+    if randomNumber == 1:
+        computerMove = 'p'
+        print('PAPER')
+    
+    
+
+
+    # Display and record win/loss/tie:
 
 
 
-# Display the player chose
 
 
-# Display the computer chose
 
-
-# Display and record win/loss/tie:
+    
