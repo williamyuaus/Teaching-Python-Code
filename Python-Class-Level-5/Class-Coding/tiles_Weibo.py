204e9cd992c89522fae90e264d3be1206ee2fc33
williamyuaus <williamyuaus@gmail.com>
2020-10-06 11:48:58 +1100

    update class code

---
+++ Python-Class-Level-5/Class-Coding/tiles_Weibo.py
@@ -0,0 +1,60 @@
+from random import *
+from turtle import *
+from base import floor, vector
+
+tiles = {}
+neighbors = [
+    vector(100, 0),
+    vector(-100, 0),
+    vector(0, 100),
+    vector(0, -100),
+]
+
+def load():
+    count = 1
+
+    for y in range(-200, 200, 100):
+        for x in range(-200, 200, 100):
+            mark = vector(x, y)
+            tiles[mark] = count
+            count += 1
+
+    tiles[mark] = None
+
+    for count in range(1000):
+        neighbor = choice(neighbors)
+        spot = mark + neighbor
+
+    
+
+def square(mark, number):
+    up()
+    goto(mark.x, mark.y)
+    down()
+
+    color('black', 'white')
+    begin_fill()
+    for count in range(4):
+        forward(99)
+        left(90)
+    end_fill()
+
+    if number is None:
+        return
+    elif number < 10:
+        forward(20)
+
+    write(number, font=('Arial', 60, 'normal'))
+    
+
+def draw():
+    for mark in tiles:
+        square(mark, tiles[mark])
+    update()
+
+setup(420, 420, 250, 250)
+hideturtle()
+tracer(False)
+load()
+draw()
+done()
