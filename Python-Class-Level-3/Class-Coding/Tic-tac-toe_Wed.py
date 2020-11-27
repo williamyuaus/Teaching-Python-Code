f5d92a5970ba969d72cf9a6a8b879935bd0b9da7
williamyu <williamyuaus@gmail.com>
2020-11-28 10:43:38 +1100

    update class code

--- Python-Class-Level-3/Class-Coding/Tic-tac-toe_Wed.py
+++ Python-Class-Level-3/Class-Coding/Tic-tac-toe_Wed.py
@@ -1,3 +1,8 @@
+
+
+
+
+
 import random
 
 def drawBoard():
@@ -7,4 +12,30 @@ def drawBoard():
     print('-+-+-')
     print(' | | ')
 
+def inputPlayerLetter():
+    letter = ''
+    while not (letter == 'X' or letter == 'O'):
+        print('Do you want to be X or O?')
+        letter = input().upper()
+
+    if letter == 'X':
+        return ['X', 'O']
+    else:
+        return ['O', 'X']
+
+def whoGoesFirst():
+    if random.randint(0, 1) == 0:
+        return 'computer'
+    else:
+        return 'player'
+    
+def makeMove(board, letter, move):
+    board[move] = letter
+
+def isWinner(bo, le):
+    return ((bo[7] == le and bo[8] == le and bo[9] == le) or
+            (bo[4] == le and bo[5] == le and bo[6] == le) or
+
 drawBoard()
+print(inputPlayerLetter())
+print(whoGoesFirst())
--- Python-Class-Level-4/Class-Coding/reversegam_Fri.py
+++ Python-Class-Level-4/Class-Coding/reversegam_Fri.py
@@ -10,7 +10,7 @@ def getNewBoard():
     return board
 
 def drawBoard(board):
-    print(' 12345678')
+    print('  12345678')
     print(' +--------+')
     for y in range(HEIGHT):
         print('%s|' % (y+1), end='')
@@ -19,4 +19,37 @@ def drawBoard(board):
         print('|%s' % (y+1))
     print(' +--------+')
     print('  12345678')
+
+def enterPlayerTile():
+    tile = ''
+    while not (tile == 'X' or tile == 'O'):
+        print('Do you want to be X or O?')
+        tile = input().upper()
+
+    if tile == 'X':
+        return ['X', 'O']
+    else:
+        return ['O', 'X']
+
+def whoGoesFirst():
+    if random.randint(0, 1) == 0:
+        return 'computer'
+    else:
+        return 'player'
+
+def getScoreOfBoard(board):
+    xscore = 0
+    oscore = 0
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            if board[x][y] == 'X':
+                xscore += 1
+            if board[x][y] == 'O':
+                oscore += 1
+    return {'X':xscore, 'O':oscore}
+
+def printScore(board, playerTile, computerTile):
+    scores = getScoreOfBoard(board)
+    print('You: %s points. Computer: %s points.' % (scores[playerTile], scores[computerTile]))
     
+drawBoard(getNewBoard())
