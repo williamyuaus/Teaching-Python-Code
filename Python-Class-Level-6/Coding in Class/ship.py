1b65148fb3c77f9cf7027b6b720e593aae1e3d4d
Will <williamyuaus@gmail.com>
2021-01-20 13:42:17 +1100

    update class code

--- Python-Class-Level-6/Coding in Class/ship.py
+++ Python-Class-Level-6/Coding in Class/ship.py
@@ -8,7 +8,7 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/cannon2.bmp')
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
