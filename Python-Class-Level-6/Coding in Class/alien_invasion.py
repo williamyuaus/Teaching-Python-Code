3c2fd96eca26ba381a2a1aec1e1d15d15598bcc5
William Yu <williamyuaus@gmail.com>
2021-01-20 17:49:41 +1100

    update class code

--- Python-Class-Level-6/Coding in Class/alien_invasion.py
+++ Python-Class-Level-6/Coding in Class/alien_invasion.py
@@ -22,17 +22,29 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+      
+    def _check_events(self):
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+    
+    def _update_screen(self):
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()        
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
--- Python-Class-Level-6/Coding in Class/ship.py
+++ Python-Class-Level-6/Coding in Class/ship.py
@@ -13,6 +13,14 @@ class Ship:
 
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Movement flag
+        self.moving_right = False
+    
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
+
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
 
