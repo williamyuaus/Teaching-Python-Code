f6e7e72c0736a92640493b6621f2b7fb514802e8
Will <williamyuaus@gmail.com>
2021-01-21 10:10:05 +1100

    update class code

--- Python-Class-Level-6/Coding in Class/ship.py
+++ Python-Class-Level-6/Coding in Class/ship.py
@@ -7,18 +7,26 @@ class Ship:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
-        self.image = pygame.image.load('images/cannon2.bmp')
+        self.image = pygame.image.load('images/ship2.bmp')
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
 
+        self.x = float(self.rect.x)
+
         # Movement flag
         self.moving_right = False
+        self.moving_left = False
     
     def update(self):
-        if self.moving_right:
-            self.rect.x += 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= self.settings.ship_speed
+        
+        self.rect.x = self.x
 
 
     def blitme(self):
