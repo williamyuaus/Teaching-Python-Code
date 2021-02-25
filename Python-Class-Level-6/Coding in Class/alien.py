7a5d0717c4e6aa0143646170306cab3caa427625
Will <williamyuaus@gmail.com>
2021-02-26 10:08:35 +1100

    update class code

--- Python-Class-Level-6/Coding in Class/alien.py
+++ Python-Class-Level-6/Coding in Class/alien.py
@@ -5,9 +5,14 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
         self.x = float(self.rect.x)
+
+    def update(self):
+        self.x += self.settings.alien_speed
+        self.rect.x = self.x
--- Python-Class-Level-6/Coding in Class/alien_invasion.py
+++ Python-Class-Level-6/Coding in Class/alien_invasion.py
@@ -28,14 +28,34 @@ class AlienInvasion:
     
     def _create_fleet(self):
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+            alien = Alien(self)
+            alien_width, alien_height = alien.rect.size
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+            self.aliens.add(alien)
+            
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
-            self._update_bullets()            
+            self._update_bullets()
+            self._update_aliens()            
             self._update_screen()
 
     def _update_bullets(self):
@@ -44,6 +64,9 @@ class AlienInvasion:
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
             #print(len(self.bullets))
+
+    def _update_aliens(self):
+        self.aliens.update()
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
