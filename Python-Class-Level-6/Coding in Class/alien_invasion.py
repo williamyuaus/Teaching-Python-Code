22d3311c3e476e220d3eec9d94961ad9b919c7f7
William Yu <williamyuaus@gmail.com>
2021-04-07 15:25:59 +1000

    update class code

--- Python-Class-Level-6/Coding in Class/alien_invasion.py
+++ Python-Class-Level-6/Coding in Class/alien_invasion.py
@@ -66,9 +66,10 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()            
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()            
             self._update_screen()
 
     def _update_bullets(self):
