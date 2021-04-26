ce408dde907c6a31f0188161147d40a7a3c6d653
Will <williamyuaus@gmail.com>
2021-04-26 19:03:05 +1000

    update class code

--- Python-Class-Level-7/Class-code/ascii_Weibo.py
+++ Python-Class-Level-7/Class-code/ascii_Weibo.py
@@ -21,4 +21,57 @@ def convertImageToAscii(fileName, cols, scale, moreLevels):
     w = W/cols
     h = w/scale
     rows = int(H/h)
+
+    print("cols: %d, rows: %d" % (cols, rows))
+    print("tile dims: %d x %d" % (w, h))
+
+    if cols > W or rows > H:
+        print("Image too small for specified cols!")
+        exit(0)
+
+    aimg = []
+
+    for j in range(rows):
+        y1 = int(j * h)
+        y2 = int((j+1) * h)
+        if j == rows - 1:
+            y2 = H
+        aimg.append("")
+        for i in range(cols):
+            x1 = int(i * w)
+            x2 = int((i + 1) * w )
+            if i == cols - 1:
+                x2 = W
+
+            img = image.crop((x1, y1, x2, y2))
+
+            avg = int(getAverageL(img))
+
+            if moreLevels:
+                gsval = gscale1[int((avg * 69) / 255)]
+            else:
+                gsval = gscale2[int((avg * 9) / 255)]
+
+            aimg[j] += gsval
+
+        return aimg
+
+def main():
+    descStr ="This program converts an image into ASCII art."
+    parser = argparse.ArgumentParser(description = descStr)
+
+    parser.add_argument('--file', dest='imgFile', required=True)
+    parser.add_argument('--scale', dest='scale', required=False)
+    parser.add_argument('--out', dest='outFile', required=False)
+    parser.add_argument('--cols', dest='cols', required=False)
+    parser.add_argument('--morelevels', dest='moreLevels', action='store_true')
+
+    args = parser.parse_args()
+
+    imgFile = args.imgFile
+    outFile = 'out.txt'
+
+    if args.outFile:
+        outFile = args.outFile
+
     
