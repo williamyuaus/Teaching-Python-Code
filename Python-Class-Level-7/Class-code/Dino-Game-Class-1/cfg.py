c2f8240c4cdc6c29fab4be17c967806659cb6732
William Yu <williamyuaus@gmail.com>
2021-06-15 15:53:56 +1000

    add dino game class 1

---
+++ Python-Class-Level-7/Class-code/Dino-Game-Class-1/cfg.py
@@ -0,0 +1,13 @@
+'''配置文件'''
+import os
+
+
+'''屏幕大小'''
+SCREENSIZE = (600, 150)
+'''FPS'''
+FPS = 60
+
+'''背景颜色'''
+BACKGROUND_COLOR = (235, 235, 235)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
---
+++ Python-Class-Level-7/Class-code/Dino-Game-Class-1/dino-game.py
@@ -0,0 +1,24 @@
+import cfg
+import sys
+import random
+import pygame
+from modules import *
+
+def main(highest_score):
+    pygame.init()
+    screen = pygame.display.set_mode(cfg.SCREENSIZE)
+    pygame.display.set_caption('T-Rex Rush')
+    GameStartInterface(screen, cfg)
+    
+    highest_score = highest_score
+    clock = pygame.time.Clock()
+    while True:
+        screen.fill(cfg.BACKGROUND_COLOR)
+        pygame.display.update()
+        clock.tick(cfg.FPS)
+
+if __name__ == '__main__':
+    highest_score = 0
+    while True:
+        flag, highest_score = main(highest_score)
+        if not flag: break
