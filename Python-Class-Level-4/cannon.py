99cd35e979805b3fd21e547c571d99992edbd57d
williamyuaus <williamyuaus@gmail.com>
2021-07-08 10:50:53 +1000

    update class code

--- Python-Class-Level-4/cannon.py
+++ Python-Class-Level-4/cannon.py
@@ -16,6 +16,8 @@ from base import vector
 ball = vector(-200, -200)
 speed = vector(0, 0)
 targets = []
+bosses = []
+writer = Turtle(visible=False)
 
 def tap(x, y):
     "Respond to screen tap."
@@ -37,6 +39,10 @@ def draw():
         goto(target.x, target.y)
         dot(20, 'blue')
 
+    for boss in bosses:
+        goto(boss.x, boss.y)
+        dot(40, 'green')
+
     if inside(ball):
         goto(ball.x, ball.y)
         dot(6, 'red')
@@ -49,9 +55,14 @@ def move():
         y = randrange(-150, 150)
         target = vector(200, y)
         targets.append(target)
+        boss = vector(200, y)
+        bosses.append(boss)
 
     for target in targets:
         target.x -= 1.5
+
+    for boss in bosses:
+        boss.x -= 10
 
     if inside(ball):
         speed.y -= 0.35
@@ -67,11 +78,24 @@ def move():
     for target in targets:
         if abs(target - ball) < 13:
             targets.remove(target)
+            ball.x = -199
+            ball.y = -199
+            speed.x = -10
+            speed.y = -10
+
+    for boss in bosses:
+        if abs(boss - ball) < 23:
+            bosses.remove(boss)
+            clear
+            speed.x = 5
 
     draw()
 
     for target in targets:
         if not inside(target):
+            writer.goto(30, 30)
+            writer.color('black')
+            writer.write('Game Over',font=("Verdana", 15, "normal"))
             return
 
     ontimer(move, 50)
