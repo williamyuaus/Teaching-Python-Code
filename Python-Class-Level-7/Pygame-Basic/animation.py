b7735c010e2a8b798f5136922a31e57efedf933d
William Yu <williamyuaus@gmail.com>
2021-07-19 20:42:04 +1000

    update class code

---
+++ Python-Class-Level-7/Pygame-Basic/animation.py
@@ -0,0 +1,46 @@
+import pygame, sys, time
+from pygame.locals import *
+
+pygame.init()
+
+#set up the window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation')
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+DOWNLEFT = 'downleft'
+DOWNRIGHT = 'downright'
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'
+
+MOVESPEED = 4
+
+b1 = {'rect': pygame.Rect(300, 80, 50, 100), 'color': RED, 'dir': UPRIGHT}
+b2 = {'rect':pygame.Rect(200, 200, 20, 20), 'color':GREEN, 'dir':UPLEFT}
+b3 = {'rect':pygame.Rect(100, 150, 60, 60), 'color':BLUE, 'dir':DOWNLEFT}
+boxes = [b1, b2, b3]
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quite()
+            sys.exit()
+
+    windowSurface.fill(WHITE)
+
+    for b in boxes:
+        if b['dir'] == DOWNLEFT:
+            b['rect'].left -= MOVESPEED
+            b['rect'].top += MOVESPEED
+
+        pygame.draw.rect(windowSurface, b['color'], b['rect'])
+
+    pygame.display.update()
+    time.sleep(0.02)
+
