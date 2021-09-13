f55df4c9b50fc4167c7d11ee582d504a6faff03a
William Yu <williamyuaus@gmail.com>
2021-09-13 18:12:36 +1000

    update class code

--- Python-Class-Level-7/Class-code/dodger-class.py
+++ Python-Class-Level-7/Class-code/dodger-class.py
@@ -13,6 +13,10 @@ BADDIEMAXSPEED = 8
 ADDNEWBADDIERATE = 6
 PLAYERMOVERATE = 5
 
+def terminate():
+    pygame.quit()
+    sys.exit()
+
 pygame.init()
 mainClock = pygame.time.Clock()
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
@@ -32,7 +36,66 @@ baddieImage = pygame.image.load('baddie.png')
 while True:
     baddies = []
     playerRect.topleft = (WINDOWWIDTH / 2, WINDOWHEIGHT - 50)
-    # play background music
+    moveLeft = moveRight = moveUp = moveDown = False
+    baddieAddCounter = 0
+    pygame.mixer.music.play(-1, 0.0)
 
     while True:
-        # key for control the game
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+
+            if event.type == KEYDOWN:
+                if event.key == K_LEFT or event.key == K_a:
+                    moveRight = False
+                    moveLeft = True
+                if event.key == K_RIGHT or event.key == K_d:
+                    moveLeft = False
+                    moveRight = True
+                if event.key == K_UP or event.key == K_w:
+                    moveDown = False
+                    moveUp = True
+                if event.key == K_DOWN or event.key == K_s:
+                    moveUp = False
+                    moveDown = True
+
+            if event.type == KEYUP:
+                if event.key == K_ESCAPE:
+                    terminate()
+                
+                if event.key == K_LEFT or event.key == K_a:
+                    moveLeft = False
+                if event.key == K_RIGHT or event.key == K_d:
+                    moveRight = False
+                if event.key == K_UP or event.key == K_w:
+                    moveUp = False
+                if event.key == K_DOWN or event.key == K_s:
+                    moveDown = False
+
+        baddieAddCounter += 1
+        if baddieAddCounter == ADDNEWBADDIERATE:
+            baddieAddCounter = 0
+            baddieSize = random.randint(BADDIEMINSIZE, BADDIEMAXSIZE)
+            newBaddie = {'rect': pygame.Rect(random.randint(0, WINDOWWIDTH - baddieSize), 0 - baddieSize, baddieSize, baddieSize),
+                         'speed': 
+
+            }
+
+
+        if moveLeft and playerRect.left > 0:
+            playerRect.move_ip(-1 * PLAYERMOVERATE, 0)
+        if moveRight and playerRect.right < WINDOWWIDTH:
+            playerRect.move_ip(PLAYERMOVERATE, 0)
+        if moveUp and playerRect.top > 0:
+            playerRect.move_ip(0, -1 * PLAYERMOVERATE)
+        if moveDown and playerRect.bottom < WINDOWHEIGHT:
+            playerRect.move_ip(0, PLAYERMOVERATE)
+            
+
+        windowSurface.fill(BACKGROUNDCOLOR)
+
+        windowSurface.blit(playerImage, playerRect)
+
+        pygame.display.update()
+
+        mainClock.tick(FPS)
