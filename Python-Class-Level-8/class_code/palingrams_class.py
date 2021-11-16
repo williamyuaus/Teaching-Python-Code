1411955764392b9e5f840ad63103b4505b42eec4
Will <williamyuaus@gmail.com>
2021-11-17 10:38:59 +1100

    update class code

---
+++ Python-Class-Level-8/class_code/palingrams_class.py
@@ -0,0 +1,16 @@
+import load_dictionary
+
+word_list = load_dictionary.load('/Users/willyu/dev/Teaching-Python-Code/Python-Class-Level-8/2of4brif.txt')
+
+def find_palingrams():
+    pali_list = []
+    for word in word_list:
+        end = len(word)
+        rev_word = word[::-1]
+        if end > 1:
+            for i in range(end):
+                if word[i:] == rev_word[:end-i] and rev_word[end-i:] in word_list:
+                    pali_list.append((word, rev_word[end-i:]))
+                if word[:i] == rev_word[end-i:] and rev_word[:end-i] in word_list:
+                    pali_list.append((rev_word[:end-i], word))
+        return pali_list
