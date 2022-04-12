caa801b6a701f94066f1f7a0078bab7fd574c6f4
Will <williamyuaus@gmail.com>
2022-04-12 14:43:50 +1000

    update class code

--- Python-Class-Level-9/Class-code/tower.py
+++ Python-Class-Level-9/Class-code/tower.py
@@ -11,7 +11,31 @@ def main():
     while True:
         displayTowers(towers)
 
+        fromTower, toTower = askForPlayerMove(towers)
+
         break
+
+def askForPlayerMove(towers):
+    print('Enter the letters of from and to ....')
+    print('(e.g. AB to moves a disk from tower A to tower B)')
+    response = input('> ').upper().strip()
+
+    if response == 'QUIT':
+        print('Thanks for playing!')
+        sys.exit()
+
+    if response not in ('AB', 'AC', 'BA', 'BC', 'CA', 'CB'):
+        print('Enter one of AB, AC, BA, BC, CA, or CB.')
+        continue
+
+    fromTower, toTower = response[0], response[1]
+
+    if len(towers[fromTower]) == 0:
+        print('You selected a tower with no disks.')
+        continue
+    elif len(tower[tower]) == 0:
+        return fromTower, toTower
+    elif 
 
 def displayTowers(towers):
     for level in range(TOTAL_DISKS, -1, -1):
